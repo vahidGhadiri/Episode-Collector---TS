f09d637c8d83a4966b4ee774d7fac1548cd79146
vahidGhadiri <whydrf@gmail.com>
2021-10-19 23:57:41 +0330

    initial commit (webpack)

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const App: React.FC = () => {
+    return (
+        <div>
+            App Component
+        </div>
+    )
+}
+
+export default App
---
+++ webpack/webpack.common.js
@@ -0,0 +1,30 @@
+const Webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "..", "build"),
+        filename: "main.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+    },
+    module: {
+        rules: [{
+            test: /\.(js|ts)x?$/,
+            exclude: /node_modules/,
+            use: [{loader: "babel-loader"}]
+        }]
+    },
+    devServer: {
+        port: 7000,
+        hot: true,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.resolve(__dirname, "..", "public/index.html")})
+    ],
+}

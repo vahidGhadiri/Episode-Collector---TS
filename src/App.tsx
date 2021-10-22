06bf6f8fdbca85159790fd0f2c121459f8958499
vahidGhadiri <whydrf@gmail.com>
2021-10-22 15:26:58 +0330

    [Bug]: Debugging structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import React, {useContext, useEffect} from "react"
 import ReactDOM from "react-dom"
-
+import axios from "axios";
 import {Store, StoreProvider} from "./context/store"
+
+import * as urls from "./config/urls";
+import {setData} from "./context/actions";
+
 import "./App.scss"
-import {fetchData} from "./context/actions";
 
 
 export const App: React.FC = () => {
-    const {state} = useContext(Store)
+    const {state, dispatch} = useContext(Store)
+
+    const fetchData = () => {
+        return new Promise((resolve, reject) => {
+            axios.get<any>(urls.FETCH_DATA)
+                .then(response => dispatch(setData(response.data._embedded.episodes)))
+                .catch(error => reject(error))
+        })
+    }
 
     useEffect(() => {
-        state.episodes.length === 0 && fetchData()
+        !state.episodes.length && fetchData()
     }, [])
 
+
     return (
-        <div>
+        <div onClick={() => console.log(state)}>
             <h1>Rick & Morty</h1>
             <p>Pick your favourite episode</p>
         </div>
@@ -26,3 +38,11 @@ ReactDOM.render(
         <App/>
     </StoreProvider>,
     document.getElementById('root'))
+
+
+export const fetchData = async () => {
+    // const {dispatch} = React.useContext(Store)
+    const data = await fetch(urls.FETCH_DATA)
+    const dataJSON = await data.json()
+    console.log(dataJSON)
+}
